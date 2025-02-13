d6482ad6c7b2022270bbf8d3d92cea6cb62953c0
Maxim Wilman <mwilman@brockhaus-ag.de>
2025-02-13 11:20:25 +0100

    v19 primeng does not work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {Button} from 'primeng/button';
+import {Slider} from 'primeng/slider';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Button, Slider, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'primeng-slider';
+  sliderMax = signal(1994);
+  sliderValue = signal(1994);
+
+  changeMax() {
+    this.sliderMax.set(1984); //after this I expect the handle should get moved or the slider should get updated and rerendered
+  }
 }
